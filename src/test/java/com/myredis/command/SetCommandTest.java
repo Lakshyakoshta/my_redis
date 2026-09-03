83924b6a784e032a0eed074ab8a68acbbf61139c
lakshya_koshta <lakshyakoshta22@gmail.com>
2026-09-03 23:53:27 +0530

    set command tests

--- src/test/java/com/myredis/command/SetCommandTest.java
+++ src/test/java/com/myredis/command/SetCommandTest.java
@@ -5,6 +5,8 @@ import com.myredis.resp.RespSimpleString;
 import com.myredis.resp.RespValue;
 import com.myredis.store.DataStore;
 import org.junit.jupiter.api.Test;
+import com.myredis.resp.RespError;
+import com.myredis.resp.RespInteger;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
@@ -41,6 +43,89 @@ class SetCommandTest {
         assertEquals(
                 "Lakshya",
                 store.get("name")
+        );
+    }
+    @Test
+    void shouldRejectWrongNumberOfArguments() {
+    
+        DataStore store = new DataStore();
+        SetCommand command = new SetCommand(store);
+    
+        RespValue[] arguments = {
+                new RespBulkString("name")
+        };
+    
+        RespValue response = command.execute(arguments);
+    
+        RespError error =
+                assertInstanceOf(RespError.class, response);
+    
+        assertEquals(
+                "ERR wrong number of arguments for 'set' command",
+                error.getMessage()
+        );
+    }
+    @Test
+    void shouldRejectNonBulkStringKey() {
+    
+        DataStore store = new DataStore();
+        SetCommand command = new SetCommand(store);
+    
+        RespValue[] arguments = {
+                new RespSimpleString("name"),
+                new RespBulkString("Lakshya")
+        };
+    
+        RespValue response = command.execute(arguments);
+    
+        RespError error =
+                assertInstanceOf(RespError.class, response);
+    
+        assertEquals(
+                "ERR key must be a bulk string",
+                error.getMessage()
+        );
+    }
+    @Test
+    void shouldRejectNonBulkStringValue() {
+    
+        DataStore store = new DataStore();
+        SetCommand command = new SetCommand(store);
+    
+        RespValue[] arguments = {
+                new RespBulkString("name"),
+                new RespInteger(100)
+        };
+    
+        RespValue response = command.execute(arguments);
+    
+        RespError error =
+                assertInstanceOf(RespError.class, response);
+    
+        assertEquals(
+                "ERR value must be a bulk string",
+                error.getMessage()
+        );
+    }
+    @Test
+    void shouldRejectNullKeyOrValue() {
+    
+        DataStore store = new DataStore();
+        SetCommand command = new SetCommand(store);
+    
+        RespValue[] arguments = {
+                new RespBulkString(null),
+                new RespBulkString("Lakshya")
+        };
+    
+        RespValue response = command.execute(arguments);
+    
+        RespError error =
+                assertInstanceOf(RespError.class, response);
+    
+        assertEquals(
+                "ERR key and value cannot be null",
+                error.getMessage()
         );
     }
 }
